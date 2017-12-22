690ec78c1b6770e59f1b51aa1c1dfa159679b608
Igor Botnaru <provodnick@gmail.com>
2017-12-22 16:38:23 +0200

    why don't work shifting function?

--- 10.php
+++ 10.php
@@ -1,9 +1,30 @@
 #!/usr/bin/env php
 <?php
-function argv_count($argv) {
-    if (count($argv) < 3) {
+
+$array = $argv;
+
+function shifting($array) {
+    array_shift($array);
+}
+
+function integer_validation($array) {
+    foreach ($array as $key => $value) {
+        if (strval(intval($value)) !== $value) {
+            die("Argument $key isn't an integer!\n\n");
+        }
+    }
+}
+
+function count_validation($array, $number) {
+    if (count($array) < $number) {
         die("Wrong arguments count!\n\n");
     }
 }
 
-argv_count($argv);
+shifting($array);
+integer_validation($array);
+count_validation($array, 4);
+
+print_r($array);
+
+echo (count($array));
