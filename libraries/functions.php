cb4b63b40a13a0fa40a5eb0d2bd2dc37e279dc36
Peter Zaharia <peton3@gmail.com>
2017-12-27 11:30:49 +0200

    Added add.php utility

--- libraries/functions.php
+++ libraries/functions.php
@@ -1,8 +1,33 @@
 <?php
 
-function checkElementsMoreThan($checkArr, $allowedNumber)
+function check_elements_more_than($checkArr, $allowedNumber)
 {
     if (count($checkArr) < $allowedNumber) {
         die("Wrong arguments count!\n\n");
+    }
+}
+
+function shifting($array)
+{
+    array_shift($array);
+    return($array);
+}
+
+function check_int_array($arr)
+{
+    foreach ($arr as $key => $value) {
+        if (strval(intval($value)) !== strval($value)) {
+            die("Argument $key is not an integer\n\n");
+        }
+    }
+}
+
+function check_and_show_help($argv, $message)
+{
+    if (isset($argv[1]) && $argv[1] === '-h') {
+        echo(PHP_EOL);
+        echo($message);
+        echo(PHP_EOL);
+        die;
     }
 }
