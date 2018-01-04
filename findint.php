4e9a6c3c5057a18ee782caa06760fa64a73a8f80
Igor Botnaru <provodnick@gmail.com>
2018-01-04 14:01:25 +0200

    Merge branch 'master' of https://github.com/peter31/exercize1
    # Conflicts:
    #	18.php
    #	add.php
    #	libraries/functions.php

---
+++ findint.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/libraries/functions.php';
+
+check_and_show_help($argv,
+    "Утилита находит количество цифр в переданном аргументом скрипта.
+Пример использования: ./explode.php 'dfg6dfg9 zd3cz 4zx2'\n");
+
+if (count($argv) !== 2 ){
+    die("Допустим только один аргумент!\n");
+}
+
+$integers = preg_replace('/[^0-9]/', '', $argv[1]);
+$result = strlen($integers);
+
+echo("Количество цифр в строке = $result\n");
+
+
