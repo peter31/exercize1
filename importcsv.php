9560bbe0f88cb9f7549eb4ba1e81be7a29358cf3
Igor Botnaru <provodnick@gmail.com>
2018-01-15 18:25:51 +0200

    Merge branch 'master' of https://github.com/peter31/exercize1
    # Conflicts:
    #	18.php
    #	add.php
    #	libraries/functions.php

---
+++ importcsv.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+
+echo("\n");
+
+// Валидация файла...
+if (($handler = @fopen($argv[1], 'r')) == FALSE) {
+    die("No such file or directory\n");
+}
+
+// Создаем массив со значениями из CSV файла...
+$users = [];
+while (($usersRow = fgetcsv($handler, 100, ',')) !== FALSE) {
+    $users[] = $usersRow;
+}
+
+fclose($handler);
+
+$link = mysqli_connect('localhost', 'root',  'qwerty', 'shop');
+
+if (!$link) {
+    printf("Can't connect to database\n");
+    exit();
+}
+
+foreach ($users as $key => $value){
+    $query = 'INSERT INTO users SET name = "' . $value[0] . '", email = "' . $value[1] . '"';
+    mysqli_query($link, $query);
+}
+
+mysqli_close($link);
+
+
+
+
+
