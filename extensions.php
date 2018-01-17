8df923578830a1d87934e458434c9c472695fe70
Igor Botnaru <provodnick@gmail.com>
2018-01-17 19:19:29 +0200

    Merge branch 'master' of https://github.com/peter31/exercize1
    # Conflicts:
    #	18.php
    #	add.php
    #	libraries/functions.php

--- extensions.php
+++ extensions.php
@@ -7,36 +7,36 @@ check_and_show_help($argv,
     "Утилита находит файлы в директории с заданым расширением переданными аргументом скрипта.
 Пример использования: ./extensions.php '/media/sf_php/' 'jpg,jpeg,txt'\n");
 
-echo("\n");
-if ( count($argv) !== 3 ){
-    die("Введите два аргумента!\n");
+echo "\n";
+if (count($argv) !== 3) {
+    exit ("Введите два аргумента!\n");
 }
 
-if (count(scandir($argv[1])) == 0) {
-    die("Папка не содержит файлов");
-}
-
-$path = $argv[1];
+$path = rtrim($argv[1], '/') . '/';
 $extensions = explode(',', $argv[2]);
 
+if (!is_dir($path)) {
+    exit ("Указанный путь не существует!\n\n");
+}
+
 $files = [];
-foreach ( scandir($path) as $value ) {
-    if ( is_file($value) ) {
+foreach (scandir($path) as $value) {
+    if (is_file($value)) {
         $files[] = $path . $value;
     }
 }
 
 $result = [];
-foreach ( $files as $value ) {
+foreach ($files as $value) {
     $parts = pathinfo($value);
-    if ( in_array($parts['extension'], $extensions) ) {
+    if (in_array($parts['extension'], $extensions)) {
         $result[] = str_replace($path, "" ,$value);
     }
 }
 
-if ( count($result) !==0 ) {
+if (count($result) !==0) {
     echo('Найдено (' . count($result) . ") файлов :\n");
-    foreach ( $result as $value ) {
+    foreach ($result as $value) {
         echo ($value . "\n");
     }
     echo("\n");
