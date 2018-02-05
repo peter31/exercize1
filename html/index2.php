21f01dc368087d8e22d3dfce103f0b389c81e286
Igor Botnaru <provodnick@gmail.com>
2018-02-05 14:34:09 +0200

    Merge branch 'master' of https://github.com/peter31/exercize1
    # Conflicts:
    #	18.php
    #	add.php
    #	libraries/functions.php

--- html/index2.php
+++ html/index2.php
@@ -7,22 +7,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
 }
 
-var_dump($_POST);
 
-//require __DIR__ . '/config.php';
-//
-//$db = mysqli_connect($database['host'], $database['user'],  $database['pass'], $database['db']);
-//
-//$query = 'SELECT * FROM users WHERE email = "' . $email . '"';
-//$insert = 'INSERT INTO users SET name = "' . $name . '", email = "' . $email . '"';
-//$update = 'UPDATE users SET name = "' . $name . '" WHERE email = "' . $email . '"';
-//
-//if (mysqli_fetch_row(mysqli_query($db, $query)) === NULL) {
-//        $added++;
-//        mysqli_query($db, $insert);
-//        echo('User was added');
-//    } else {
-//        mysqli_query($db, $update);
-//        echo('User was updated');
-//}
-//mysqli_close($db);
+require __DIR__ . '/config.php';
+
+$db = mysqli_connect($database['host'], $database['user'],  $database['pass'], $database['db']);
+
+$query = 'SELECT * FROM users WHERE email = "' . $email . '"';
+$insert = 'INSERT INTO users SET name = "' . $name . '", email = "' . $email . '"';
+$update = 'UPDATE users SET name = "' . $name . '" WHERE email = "' . $email . '"';
+
+if (mysqli_fetch_row(mysqli_query($db, $query)) === NULL) {
+    $added++;
+    mysqli_query($db, $insert);
+    echo('User was added');
+} else {
+    mysqli_query($db, $update);
+    echo('User was updated');
+}
+mysqli_close($db);
